75c21c313fecbbb453836d59c6c9a3a9bd0e636e
MAM <muzammilmohd538@gmail.com>
2024-09-16 20:54:43 +0530

    new start

--- index.js
+++ index.js
@@ -2,35 +2,35 @@ const wraper = document.querySelector(".wraper");
 
 const designList = [
   {
-    title: "Glowing Neon Buttons",
+    title: "Personal Portfolio Website",
     img: "./images/1.png",
-    url: "./glowing-neon-button/gnb.html"
+    url: "./beginner/personal-portfolio/ppfw.html"
   },
   {
-    title: "Neumorphism Login Form",
+    title: "Simple To-Do List App",
     img: "./images/2.png",
-    url: "./glowing-neon-button/nlf.html"
+    url: "./"
   }
   
 ];
 
 designList.forEach((item, index) => {
   let card = document.createElement("div");
-  let img = document.createElement("img");
+  // let img = document.createElement("img");
   let heading = document.createElement("h3");
   let para = document.createElement("p");
   let link = document.createElement("a");
 
   card.classList.add("card");
-  img.classList.add("card-img");
-  img.src = item.img;
+  // img.classList.add("card-img");
+  // img.src = item.img;
   heading.textContent = item.title;
   para.textContent = `Day - ${index + 1}`;
   link.href = item.url;
   link.textContent = "View Now";
   link.classList.add("btn");
   link.target = "_blank";
-  card.appendChild(img);
+  // card.appendChild(img);
   card.appendChild(heading);
   card.appendChild(para);
   card.appendChild(link);
